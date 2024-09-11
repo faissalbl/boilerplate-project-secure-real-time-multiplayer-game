be957bbcebb0e5bb870f1e5a12cf693cfc0218df
Faissal Lauar <faissalbl@gmail.com>
2024-09-11 01:51:26 +0000

    initial dev

--- public/game.mjs
+++ public/game.mjs
@@ -4,3 +4,12 @@ import Collectible from './Collectible.mjs';
 const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
+
+const player1 = new Player({ x: 0, y: 0, score: 0, id: 'player1' });
+
+drawPlayer(player1);
+
+function drawPlayer(player) {
+    context.fillStyle = "white";
+    context.fillRect(player.x, player.y, 20, 20);
+}
